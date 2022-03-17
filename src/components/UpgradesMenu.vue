f6f7b2041ee5b1462b33876134f0212b183725cf
AlejandroDVDSM <bysalexx@gmail.com>
2022-03-17 15:58:22 +0000

    feat: the construction site has started to develop

--- src/components/UpgradesMenu.vue
+++ src/components/UpgradesMenu.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>Upgrades</div>
+  <div>Upgrades
+
+    <Construction class="construction" :name="names[0]" :price="prices[0]" :iGot="n[0]"></Construction>
+    <Construction :name="names[1]" :price="prices[1]" :iGot="n[1]"></Construction>
+    <Construction :name="names[2]" :price="prices[2]" :iGot="n[2]"></Construction>
+    <Construction :name="names[3]" :price="prices[3]" :iGot="n[3]"></Construction>
+  </div>
 </template>
 
 <script lang="ts">
-export default {
+import { defineComponent } from 'vue'
+import Construction from "./UpgradesMenuComponents/Construction.vue"
 
-}
+export default defineComponent({
+  components: {
+    Construction
+  },
+  data() {
+    return {
+      names: ["Lumberjack", "Shop", "Factor", "Rocket"],
+      prices: [10, 100, 1000, 15000],
+      n: [0, 0, 0, 0],
+    }
+  }
+})
 </script>
 
 <style lang="sass" scoped>
 div 
-    background: aqua
-    width: 20%
+  background: aqua
+  width: 30%
 </style>
---
+++ src/components/UpgradesMenuComponents/Construction.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="constructionContainer">
+        <div>
+            <h1>{{ name }}</h1>
+        </div>
+
+        <div>
+            <p>{{ price }}</p>
+        </div>
+
+        <div>
+           <p>{{ iGot }}</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        image: Image,
+        name: String,
+        price: Number,
+        iGot: Number // How many of X construction do I have
+    }
+})
+</script>
+
+<style lang="sass" scoped>
+.constructionContainer
+    width: auto
+    display: flex
+    justify-content: space-around
+    border: 5px solid black
+    margin: 1em 1em
+
+</style>

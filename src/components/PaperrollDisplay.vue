9fa397d3fd13ca6760e708e0822fa8517f7ca1d6
AlejandroDVDSM <bysalexx@gmail.com>
2022-03-17 11:51:25 +0000

    feat: when you click on the paperroll, the puntuation is increased and is display on the screen too

--- src/components/PaperrollDisplay.vue
+++ src/components/PaperrollDisplay.vue
@@ -1,14 +1,38 @@
 <template>
-  <div>Paperroll</div>
+  <div>
+    <h1> {{ getPointsPerClick() }} pieces of paper rolls</h1>
+    <img @click="addPointsPerClick()" src="../../img/paperroll.png" width="275" height="275">
+  </div>
 </template>
 
 <script lang="ts">
-export default {
+import { defineComponent } from 'vue'
 
-}
+export default defineComponent({
+  data() {
+    return {
+      pointsPerClick: 0,
+      pointsPerSecond: 0,
+    };
+  },
+  methods: {
+    addPointsPerClick() {
+      this.pointsPerClick++
+    },
+    getPointsPerClick() {
+      return this.pointsPerClick
+    }
+  }
+})
+
 </script>
 
 <style lang="sass" scoped>
 div 
   width: 30%
+  display: flex
+  flex-direction: column
+  justify-content: center
+
+
 </style>

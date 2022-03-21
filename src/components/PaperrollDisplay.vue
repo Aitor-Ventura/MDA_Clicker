bce737fd88e9173dcc6290acbe01dd73b5e3070d
Manuel González <mangonzalezber@gmail.com>
2022-03-21 10:33:34 +0000

    :(

--- src/components/PaperrollDisplay.vue
+++ src/components/PaperrollDisplay.vue
@@ -5,7 +5,12 @@
     <div class="arriba">per second: {{ abbreviateNumber(main.pointsPerSecond, 2) }}</div>
     <img class="paperroll arriba" @click="main.addPointsPerClick" src="../../img/paperroll.png" width="275" height="275" />
   </div>
-  <div class="wave"></div>
+  <div class="animation">
+    <div class="wave"></div>
+    <div class="wave2"></div>
+  </div>
+
+
 </template>
 
 <script setup lang="ts">
@@ -18,6 +23,7 @@ main.addPointsPerSecond();
 </script>
 
 <style lang="sass" scoped>
+
 .main
   width: 30%
   height: 100%
@@ -43,15 +49,29 @@ main.addPointsPerSecond();
   bottom: 0
   left: 20%
   width: 30%
-  height: 120vh
+  height: 100%
   background: url("../assets/svg/layerBlancoAGris2 inverted.svg")
+  background-repeat: no-repeat
+
+.animation
   animation: animate 5s linear infinite
   opacity: 1
   animation-delay: 0s
   bottom: 0
   animation-direction: alternate
 
-.arriba
+.wave2
+  position: absolute
+  z-index: -2
+  top: 8.7%
+  left: 20%
+  width: 30%
+  height: 100%
+  background: url("../assets/svg/layerBlancoAGris2 inverted.svg")
+  background-repeat: no-repeat
+  transform: scaleY(-1)
+
+  .arriba
   z-index: 10
 
 
@@ -66,4 +86,11 @@ main.addPointsPerSecond();
 
   100%
     background-position-y: 0
+
+@media (max-height: 800px) 
+  .wave2  
+    display: none
+  
+
+  
 </style>

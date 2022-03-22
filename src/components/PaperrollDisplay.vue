5a1f13da463f7216f82aaa5f6eab55bf6fd4114a
AntonioMMartel <60984007+AntonioMMartel@users.noreply.github.com>
2022-03-22 14:42:33 +0000

    Wave problems solved

--- src/components/PaperrollDisplay.vue
+++ src/components/PaperrollDisplay.vue
@@ -5,10 +5,7 @@
     <div class="arriba">per second: {{ abbreviateNumber(main.pointsPerSecond, 2) }}</div>
     <img class="paperroll arriba" @click="main.addPointsPerClick" src="/src/assets/png/paperroll.png" width="275" height="275" />
   </div>
-  <div class="animation">
-    <div class="wave"></div>
-    <div class="wave2"></div>
-  </div>
+  <div class="wave"></div>
 </template>
 
 <script setup lang="ts">
@@ -44,32 +41,25 @@ main.addPointsPerSecond();
 .wave
   position: absolute
   z-index: -2
-  bottom: 0
+  top: 0
   left: 20%
   width: 30%
   height: 100%
-  background: url("../assets/svg/layerBlancoAGris2 inverted.svg")
   background-repeat: no-repeat
-
-.animation
   animation: animate 5s linear infinite
   opacity: 1
   animation-delay: 0s
   bottom: 0
   animation-direction: alternate
+  @media screen and (max-height: 2200px)
+    background: url("../assets/svg/2.svg")
+    background-repeat: no-repeat
+  @media screen and (min-height: 2200px)
+    background: url("../assets/svg/2.svg")
+    background-repeat: no-repeat
 
-.wave2
-  position: absolute
-  z-index: -2
-  top: 8.7%
-  left: 20%
-  width: 30%
-  height: 100%
-  background: url("../assets/svg/layerBlancoAGris2 inverted.svg")
-  background-repeat: no-repeat
-  transform: scaleY(-1)
 
-  .arriba
+.arriba
   z-index: 10
 
 .paperroll
@@ -83,11 +73,4 @@ main.addPointsPerSecond();
 
   100%
     background-position-y: 0
-
-@media (max-height: 800px) 
-  .wave2  
-    display: none
-  
-
-  
 </style>

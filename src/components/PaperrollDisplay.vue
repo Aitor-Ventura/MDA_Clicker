d72bc50d7b708b2087f09c3c03f3fe77c793e55d
AlejandroDVDSM <bysalexx@gmail.com>
2022-03-28 19:36:20 +0100

    and another one

--- src/components/PaperrollDisplay.vue
+++ src/components/PaperrollDisplay.vue
@@ -5,7 +5,7 @@
       <div class="arriba text-center text-3xl">{{ abbreviateNumber(+main.totalPoints.toFixed(2), 2) }} papers</div>
       <div class="arriba text-center text-lg">per second: {{ abbreviateNumber(main.pointsPerSecond, 2) }}</div>
     </div>
-    <img class="paperroll arriba" @click="main.addPointsPerClick" src="/src/assets/png/paperroll.png" width="275" height="275" />
+    <img class="paperroll arriba" @click="main.addPointsPerClick" :src=main.getImages()[0] width="275" height="275" />
   </div>
   <div class="wave-large"></div>
 </template>
